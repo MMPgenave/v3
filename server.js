c3ba1bcccd6eaddab94fb8f1aec659d9e0c4637d
MMPgenave <mohammadmp14@gmail.com>
2024-07-02 17:15:34 +0330

    change localhost and port

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const { createServer } = require("node:http");
 const next = require("next");
 const { Server } = require("socket.io");
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
+const hostname = "https://subtle-queijadas-5280a0.netlify.app/";
+const port = "";
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 app.prepare().then(() => {
