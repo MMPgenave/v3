7e769476625575509edc8ae0c71e4ee018177de2
MMPgenave <mohammadmp14@gmail.com>
2024-09-03 08:42:00 +0330

    fix a fucking error Error: react_redux__WEBPACK_IMPORTED_MODULE_0__.useDispatch.withTypes is not a function

--- app/UI/components/widgets/profile/profile-banner.js
+++ app/UI/components/widgets/profile/profile-banner.js
@@ -1,3 +1,4 @@
+"use client";
 import banner from "@/app/lib/assets/img/banner.png";
 import { BannerAddButton as AddButton, Banner, AddImageButton, UserName, ProfileAvatar } from "../../base";
 import { AvatarContainer, ProfileBannerContainer as Container } from "@/app/UI/layout";
