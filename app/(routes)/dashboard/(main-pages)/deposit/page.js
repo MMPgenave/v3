c10084f62fd15c0d3e0b75ab504bf874244c814a
MMPgenave <mohammadmp14@gmail.com>
2025-02-07 20:28:36 +0330

    prepare the app for build

--- app/(routes)/dashboard/(main-pages)/deposit/page.js
+++
@@ -1,8 +0,0 @@
-import DepositHTMLComponent from "@/app/UI/components/base/HTMLComponents/Deposit";
-export default function Page() {
-  return (
-    <div className="p-4">
-      <DepositHTMLComponent filename="deposit.html" />
-    </div>
-  );
-}
